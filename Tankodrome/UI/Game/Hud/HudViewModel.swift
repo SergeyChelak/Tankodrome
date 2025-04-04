554fe28d29705ed79f0e3c5ef668f29d1522778c
Sergey Chelak <sergey.chelak@gmail.com>
2025-04-04 22:55:22 +0300

    update hud

--- Tankodrome/UI/Game/Hud/HudViewModel.swift
+++ Tankodrome/UI/Game/Hud/HudViewModel.swift
@@ -5,22 +5,48 @@
 //  Created by Sergey on 01.03.2025.
 //
 
+import Combine
 import Foundation
 
 final class HudViewModel: ObservableObject {
+    private var cancellables: Set<AnyCancellable> = []
     private let gameFlow: GameFlow
     @Published
     private(set) var healthPercentage: CGFloat = 0.0
         
     init(gameFlow: GameFlow) {
         self.gameFlow = gameFlow
+        gameFlow.eventPublisher
+            .publisher
+            .receive(on: DispatchQueue.global())
+            .sink { [weak self] _ in
+                self?.updateHud()
+            }
+            .store(in: &cancellables)
     }
     
     var healthText: String {
         String(format: "Health: %.0f%%", 100.0 * healthPercentage)
     }
     
+    private var gameScene: GameScene {
+        gameFlow.gameScene
+    }
+    
     func onPauseTap() {
-        gameFlow.gameScene.pushSpecialInstruction(.terminate)
+        gameScene.pushSpecialInstruction(.terminate)
+    }
+    
+    // called on background thread
+    private func updateHud() {
+        guard let component = gameScene.getComponent(of: HudDataComponent.self) else {
+            return
+        }
+        let data = component.value
+        if data.playerHealth != healthPercentage {
+            Task { @MainActor in
+                healthPercentage = data.playerHealth
+            }
+        }
     }
 }
